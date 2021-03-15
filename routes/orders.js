1fe4dc8d740658f87dbf7d484aff9c90ea76f6fa
hardikpoudel <hardikpoudel25@gmail.com>
2021-03-15 14:11:57 +0545

    backend completed

--- routes/orders.js
+++ routes/orders.js
@@ -1,14 +1,149 @@
 const { Order } = require("../models/order");
 const express = require("express");
 const router = express.Router();
+const { OrderItem } = require("../models/order-item");
 
 router.get(`/`, async (req, res) => {
-  const orderList = await Order.find();
+  const orderList = await Order.find()
+    .populate("user", "name")
+    .sort({ dateOrdered: -1 });
 
   if (!orderList) {
     res.status(500).json({ success: false });
   }
   res.send(orderList);
+});
+
+//api for finding by ID
+router.get(`/:id`, async (req, res) => {
+  const order = await Order.findById(req.params.id)
+    .populate("user", "name")
+    .populate({
+      path: "orderItems",
+      populate: {
+        path: "product",
+        populate: "category",
+      },
+    });
+
+  if (!order) {
+    res.status(500).json({ success: false });
+  }
+  res.send(order);
+});
+
+router.post("/", async (req, res) => {
+  const orderItemsIds = Promise.all(
+    //loop the every orderitem and save it in the database
+    req.body.orderItems.map(async (orderItem) => {
+      let newOrderItem = new OrderItem({
+        quantity: orderItem.quantity,
+        product: orderItem.product,
+      });
+
+      newOrderItem = await newOrderItem.save();
+      //the id of the orderItem is only returned
+      return newOrderItem._id;
+    })
+  );
+
+  //resolving the promises using the await function
+  const orderItemsIdsResolved = await orderItemsIds;
+  //calulating total price of the each item orders
+  //the total price have value stored in array, for each item price
+  const totalPrices = await Promise.all(
+    orderItemsIdsResolved.map(async (orderItemId) => {
+      const orderItem = await OrderItem.findById(orderItemId).populate(
+        "product",
+        "price"
+      );
+      const totalPrice = orderItem.product.price * orderItem.quantity;
+      return totalPrice;
+    })
+  );
+
+  //combining every item of array to get the total price
+  const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
+  let order = new Order({
+    orderItems: orderItemsIdsResolved,
+    shippingAddress1: req.body.shippingAddress1,
+    shippingAddress2: req.body.shippingAddress2,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    phone: req.body.phone,
+    status: req.body.status,
+    totalPrice: totalPrice,
+    user: req.body.user,
+  });
+  order = await order.save();
+
+  // if(!order)
+  // return res.status(400).send('the order cannot be created!')
+
+  res.send(order);
+});
+
+//to change the status of the product from the pending to shipped
+
+router.put("/:id", async (req, res) => {
+  const order = await Order.findByIdAndUpdate(
+    req.params.id,
+    {
+      status: req.body.status,
+    },
+    { new: true }
+  );
+
+  if (!order) return res.status(400).send("the order cannot be update!");
+
+  res.send(order);
+});
+
+router.delete("/:id", (req, res) => {
+  Order.findByIdAndRemove(req.params.id)
+    .then(async (order) => {
+      if (order) {
+        await order.orderItems.map(async (orderItem) => {
+          await OrderItem.findByIdAndRemove(orderItem);
+        });
+        return res
+          .status(200)
+          .json({ success: true, message: "the order is deleted!" });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: "order not found!" });
+      }
+    })
+    .catch((err) => {
+      return res.status(500).json({ success: false, error: err });
+    });
+});
+
+router.get("/get/totalsales", async (req, res) => {
+  const totalSales = await Order.aggregate([
+    //return id to null
+    //mongoose cannot return the object without any ID
+    { $group: { _id: null, totalsales: { $sum: "$totalPrice" } } },
+  ]);
+
+  if (!totalSales) {
+    return res.status(400).send("The order sales cannot be generated");
+  }
+
+  res.send({ totalsales: totalSales.pop().totalsales });
+});
+
+router.get(`/get/count`, async (req, res) => {
+  const orderCount = await Order.countDocuments((count) => count);
+
+  if (!orderCount) {
+    res.status(500).json({ success: false });
+  }
+  res.send({
+    orderCount: orderCount,
+  });
 });
 
 module.exports = router;
